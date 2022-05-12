1b33261d15d3149e6fcc5dc3725a55a2e1016c0d
Decryptus007 <domnick080@outlook.com>
2022-05-12 20:44:44 +0100

    fix overscroll

--- js/script.js
+++ js/script.js
@@ -7,11 +7,13 @@ let sliderSwitch = false
 slider.addEventListener('click', () => {
     if (!sliderSwitch) {
         body.style.transform = 'translateX(75%)'
+        body.classList.add('stop-scrolling')
         slider.classList.remove('fa-sliders')
         slider.classList.add('fa-close')
         sliderSwitch = true
     } else {
         body.style.transform = 'translateX(0)'
+        body.classList.add('stop-scrolling')
         slider.classList.remove('fa-close')
         slider.classList.add('fa-sliders')
         sliderSwitch = false
